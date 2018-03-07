3e86d216f40b7b3b54e6ed9a748b2d8c812a379d
Paul Billings <paulbillings101@yahoo.co.uk>
2018-03-07 23:09:45 +0000

    Added register route

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ var express         = require("express"),
     User            = require("./models/user"),
     seedDB          = require("./seeds");
 
+// Passport Configuration
+app.use(require("express-session")({
+    secret: "Coding is simply the best!",
+    resave: false,
+    saveUninitialized: false 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -97,6 +109,31 @@ app.post("/campgrounds/:id/comments", function(req, res){
             });
         }
     });
+});
+
+// *********************
+//      Auth Routes
+// *********************
+
+// show register form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+// post register logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render("register");
+       }
+       passport.authenticate("local")(req, res, function(){
+          res.redirect("/campgrounds"); 
+       });
+    });
+  
+   
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
