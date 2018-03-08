f9870fd7fbfd3356e3fff8ebfb434e411d5f612e
Paul Billings <paulbillings101@yahoo.co.uk>
2018-03-08 22:11:17 +0000

    Added login page and route

--- app.js
+++ app.js
@@ -136,6 +136,20 @@ app.post("/register", function(req, res){
    
 });
 
+// show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// login post route logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+        
+    }), function(req, res){
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("YelpCamp server has started!!"); 
 });
