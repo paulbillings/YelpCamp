f559ff644534e0e3ce04ba3b4dea56fe660e646f
Paul Billings <paulbillings101@yahoo.co.uk>
2018-03-09 13:40:47 +0000

    Added logout route and a isLoggedIn function

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get("/campgrounds/:id", function(req, res){
 // ===============
 
 // new comment
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
         Campground.findById(req.params.id, function(err, campground){
            if(err){
                 console.log(err);
@@ -92,7 +92,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 });
 
 // create comment
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -149,6 +149,19 @@ app.post("/login", passport.authenticate("local",
         
     }), function(req, res){
 });
+
+// logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("YelpCamp server has started!!"); 
