2d622e19a6c4a7e6e66ce019253b8b1e290cbdc6
Paul Billings <paulbillings101@yahoo.co.uk>
2018-03-11 21:41:22 +0000

    Author and comment are now associated

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
-seedDB();
+// seed the database
+// seedDB();
 
 // Passport Configuration
 app.use(require("express-session")({
