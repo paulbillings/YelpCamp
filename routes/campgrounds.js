26a2727eeca494cbea1bd90f8c687af12c3c0a1b
Paul Billings <paulbillings101@yahoo.co.uk>
2018-03-17 18:28:21 +0000

    Associated campgrounds with user who created it.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,22 +15,27 @@ router.get("/", function(req, res){
 });
 
 // create campground
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     Campground.create(newCampground, function(err, newlyCreated){
        if(err){
            console.log(err);
        } else {
+           console.log(newlyCreated);
            res.redirect("/campgrounds");
        }
     });
 });
 
 // new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
    res.render("campgrounds/new");
 });
 
@@ -46,5 +51,13 @@ router.get("/:id", function(req, res){
     });
    
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
