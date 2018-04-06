96b9189b6be4d0ea6f63c05c40df8c38fc71e388
Paul Billings <paulbillings101@yahoo.co.uk>
2018-04-06 11:13:54 +0000

    Updated layout of login/register forms

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res){
 
 // show register form
 router.get("/register", function(req, res){
-   res.render("register"); 
+   res.render("register", {page: "register"}); 
 });
 
 // post register logic
@@ -18,9 +18,8 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
        if(err){
-            req.flash("error", err.message);
             console.log(err);
-            return res.redirect("register");
+            return res.render("register", {error: err.message});
        }
        passport.authenticate("local")(req, res, function(){
             req.flash("success", "Welcome to YelpCamp " + user.username); 
@@ -33,7 +32,7 @@ router.post("/register", function(req, res){
 
 // show login form
 router.get("/login", function(req, res){
-    res.render("login");
+    res.render("login", {page: "login"});
 });
 
 // login post route logic
