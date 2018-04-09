23830406ddb3416e4f6f1fead336c58a253dc167
Paul Billings <paulbillings101@yahoo.co.uk>
2018-04-09 20:52:41 +0000

    add admin code to .env file

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get("/register", function(req, res){
 // post register logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
-    if(req.body.adminCode === "secretcode123") {
+    if(req.body.adminCode === process.env.SECRETCODE) {
         newUser.isAdmin = true;
     }
     User.register(newUser, req.body.password, function(err, user){
