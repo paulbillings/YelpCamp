3aee86331583c1158332be2968ea4e70aa27b044
Paul Billings <paulbillings101@yahoo.co.uk>
2018-04-09 20:34:29 +0000

    add admin role

--- middleware/index.js
+++ middleware/index.js
@@ -13,7 +13,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
             res.redirect("back");
         } else {
             //does user own the campground?
-            if(foundCampground.author.id.equals(req.user._id)){
+            if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
                 req.campground = foundCampground;
                 next();
             } else {
@@ -37,7 +37,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
             res.redirect("back");
         } else {
             //does user own the comment?
-            if(foundComment.author.id.equals(req.user._id)){
+            if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
                 req.comment = foundComment;
                 next();
             } else {
